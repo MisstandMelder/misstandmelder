a1175c548e6f9be43b418d428ff97a688ce16a07
MisstandMelder <evert.lenos@gmail.com>
2025-05-07 20:37:45 +0200

    Debug Grok API route, add test route, install shadcn components, and fix Next.js version

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,16 @@
-'use client'
+'use client';
 
-import * as React from 'react'
+import * as React from 'react';
+import { Suspense } from 'react';
 import {
   ThemeProvider as NextThemesProvider,
   type ThemeProviderProps,
-} from 'next-themes'
+} from 'next-themes';
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <Suspense fallback={<div>Loading theme...</div>}>
+      <NextThemesProvider {...props}>{children}</NextThemesProvider>
+    </Suspense>
+  );
 }
