84b901b300bd62a69a7c37ada5b0468dd497fbd8
MisstandMelder <evert.lenos@gmail.com>
2025-05-22 09:55:41 +0200

    Update melding-formulier.tsx

--- components/melding-formulier.tsx
+++ components/melding-formulier.tsx
@@ -184,7 +184,7 @@ export function MeldingFormulier({ onMeldingGenerated }: MeldingFormulierProps) 
                         <MapPin className="absolute left-3 top-2.5 h-5 w-5 text-muted-foreground" />
                       </div>
                     </FormControl>
-                    <FormDescription>Geef de naam van de organisatie, instelling of plaats waar de misstand zich heeft voorgedaan.</FormDescription>
+                    <FormDescription>Naam van organisatie, instelling of locatie van de misstand</FormDescription>
                     <FormMessage />
                   </FormItem>
                 )}
