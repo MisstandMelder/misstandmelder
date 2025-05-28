525554c7a2ff4387dfc458b71afd0c87173845b3
MisstandMelder <evert.lenos@gmail.com>
2025-05-28 15:36:15 +0200

    Update google-analytics.tsx

--- components/google-analytics.tsx
+++ components/google-analytics.tsx
@@ -1,16 +1,17 @@
+// components/google-analytics.tsx
 "use client"
 
 import { useEffect } from "react"
 
 export default function GoogleAnalytics() {
   useEffect(() => {
-    // Voeg Google Analytics script dynamisch toe
+    // Voeg het Google Analytics script dynamisch toe
     const script = document.createElement("script")
     script.src = "https://www.googletagmanager.com/gtag/js?id=G-S1H5JG69T0"
     script.async = true
     document.head.appendChild(script)
 
-    // Initialiseer GA
+    // Init Google Analytics
     window.dataLayer = window.dataLayer || []
     function gtag(...args: any[]) {
       window.dataLayer.push(args)
