91ef80a52733dde2929da5dd21d1b40294fee8b0
MisstandMelder <evert.lenos@gmail.com>
2025-06-18 17:23:40 +0200

    Update meldingen.js

--- pages/meldingen.js
+++ pages/meldingen.js
@@ -1,1 +1,21 @@
 
+// pages/meldingen.js
+import React from 'react';
+
+const Meldingen = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Alle Meldingen</h1>
+      {/* Hier kunnen meldingen uit een database of mock-data worden geladen */}
+      <div className="space-y-4">
+        {/* Voorbeeldmeldingen */}
+        <div className="p-4 border rounded shadow">Melding 1</div>
+        <div className="p-4 border rounded shadow">Melding 2</div>
+        <div className="p-4 border rounded shadow">Melding 3</div>
+        {/* Voeg eventueel een paginering of infinite scroll toe */}
+      </div>
+    </div>
+  );
+};
+
+export default Meldingen;
